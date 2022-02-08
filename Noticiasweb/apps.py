b196ab2881e95145279e09361914a83ef82f05a4
Damian <dam.oscar.amigo@gmail.com>
2022-02-07 18:13:32 -0600

    archivos de we cargados

--- Noticiasweb/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NoticiaswebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Noticiasweb'
--- Preciosweb/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrecioswebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Preciosweb'
